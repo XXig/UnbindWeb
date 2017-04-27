eaa637ba78a2d513a013b6339ae69a3ea565d97c
username <397204790@qq.com>
2017-04-27 17:16:00 +0800

    upd

--- src/js/style.js
+++ src/js/style.js
@@ -1,40 +1,33 @@
-// document.addEventListener('touchmove', function (event) {
-// 	event.preventDefault();
-// }, false);
-// $(window).on('scroll.elasticity',function(e) {
-// 	e.preventDefault();
-// }).on('touchmove.elasticity',function(e) {
-// 	e.preventDefault();
-// });
-$(window).on('resize', function() {
-	document.documentElement.style.fontSize = $(document.documentElement).width()/3.75 + 'px';
-});
-$(".box ul li").on("click",function(){
-	window.location.href = "http://t.cn/" + $(this).attr("url");
-})
-var loader = new resLoader({
-	resources : [
-	'../images/loading.png',
-	'../images/arrow.png',
-	'../images/bg.jpg'
-	],
-	onProgress : function(current, total){
-		var percent = Math.round(current/total*100);
-		$('.load span').text(percent+"%");
-	},
-	onComplete : function(total){
-		$('.load').addClass("fadeOut").remove();
-		$('.start').show().addClass("fadeIn");
-		$('.box ul').show().addClass("fadeIn");
-	}
-}); 
-function g(t) {
-	return parseInt(t, 10) + "deg"
-}
-window.addEventListener("deviceorientation",
-	function(ev) {
-		var e = ev.gamma;
-		if (e) {
-			$(".box ul").css("transform", "rotateY("+ g(.35 * e) + ")")
-		};
-	},true);
+				var l=window.location.hash;
+				$(l).find("li").addClass("on"),
+				$(l).siblings().find("li").removeClass("on");
+				$("a[href='" + l +"']").addClass("on").parent("li").siblings().find("a").removeClass("on");
+				// $(function(){
+					$(".main ul a").click(function(){
+						// return false;
+					})
+					$(".nav li a").click(function(){
+						$(this).addClass("on").parent("li").siblings().find("a").removeClass("on");
+						var $main=$($(this).attr("href"));
+						$main.find("li").addClass("on"),
+						$main.siblings().find("li").removeClass("on");
+					})
+					$(".btn").click(function(){
+						console.log('test');
+						for(var i=0;i<$(l +" li a");i++){
+							var hammeraLi = new Hammer($(l +" li a")[i]);
+							hammeraLi.trigger("click");
+						}
+						// setTimeout(function(){
+						// 	console.log();
+						// },0)
+						// $.each(divSelects,function() {
+						// 	$(this).click()
+						// })
+						// $(l +" li a").each(function(){
+							// $(this).click()
+							// var url=$(this).attr('href');
+							// window.location.href=url; 
+						// }); 
+					})
+				// })
