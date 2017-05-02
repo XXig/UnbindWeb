30cffddfc109fc811b57ea8d178879a2a8b45f1d
username <397204790@qq.com>
2017-05-02 14:46:05 +0800

    upd

--- src/js/style.js
+++ src/js/style.js
@@ -1,30 +1,37 @@
-				var l=window.location.hash;
-				$(l).find("li").addClass("on"),
-				$(l).siblings().find("li").removeClass("on");
-				$("a[href='" + l +"']").addClass("on").parent("li").siblings().find("a").removeClass("on");
-				$(".nav li a").click(function(){
-					$(this).addClass("on").parent("li").siblings().find("a").removeClass("on");
-					var $main=$($(this).attr("href"));
-					$main.find("li").addClass("on"),
-					$main.siblings().find("li").removeClass("on");
-				})
-				$(".main ul li").click(function(){
-					$(this).toggleClass("on")
-				})
-				
-				$(".btn a").click(function(){
-					var a=$(".main ul").find("li.on>a");
-					for (var i = 0; i < a.length; i++) {
-						(function(m){
-							var item = a[m],  
-							$item = $(item);
-							setTimeout(function(){
-								console.log(item.href);
-								// item.click()
-								// window.open(item.href);
-								// $item.trigger("click")
-								$item.parent("li").addClass("on")
-							},m*1000)
-						})(i)
-					}
-				})
+	document.body.addEventListener("touchmove",function(a){a.preventDefault()},!1);
+	document.querySelector(".main").addEventListener("touchmove",function(a){a.stopPropagation()},!1);
+	var l=window.location.hash;
+	var clickEvent = (document.ontouchstart!==null) ? 'click' : 'touchstart';
+	var bg={
+		c:!0,
+		click:function(){
+			this.c&&($(".main").css("overflow",""),$(".bg").hide(),$(".nav").css("height",""))
+		}
+	};
+	$(l).find("li").addClass("on");
+	$(l).siblings().find("li").removeClass("on");
+	$("a[href='" + l +"']").addClass("on").parent("li").siblings().find("a").removeClass("on");
+	$(".nav li a").on("click",function(){
+		bg.c=!0,bg.click(),
+		$(this).addClass("on").parent("li").siblings().find("a").removeClass("on");
+		var $main=$($(this).attr("href"));
+		$main.find("li").addClass("on"),
+		$main.siblings().find("li").removeClass("on");
+	}),
+	$(".main ul li").on("click",function(){
+		$(this).toggleClass("on")
+	}),
+	$(".nav-s").on(clickEvent,function(){
+		if (bg.c) {
+			$(".main").css("overflow","hidden"),
+			$(".nav").css("height","400px"),
+			$(".bg").show(),
+			bg.c=!1;
+		}
+		else{
+			bg.c=!0;bg.click()
+		}
+	}),
+	$(".bg").on(clickEvent,function(){
+		bg.c=!0;bg.click()
+	});
